ed95aab93386b0554a8ee7b6ff9335b2dcf0ba5c
Vivek Ingale <155470579+Vivekingale7@users.noreply.github.com>
2024-08-14 12:12:48 +0530

    Add files via upload

---
+++ String1.java
@@ -0,0 +1,15 @@
+class StringDemo{
+
+
+
+    public static void main(String[] args){
+
+        String str = "Vivek";
+
+        int x = str.length();
+        
+        char[] ch = new char[]{};
+ 
+    }
+    
+}
---
+++ program1.java
@@ -0,0 +1,12 @@
+class Viv {
+    static int y = 20;
+    public static void main(String[] args){
+
+        int x = 10 ;
+
+        System.out.println(x);
+        System.out.println(y);
+
+    } 
+    
+}
